b9d7f7eb785fcc302c20a9f2967229fa7727b2a7
luca.mamprin <lucamamprin@users.noreply.github.com>
2021-11-24 13:06:20 +0100

    Add wallpaper and `cosmetic.sh` script to apply dark mode and wallpaper

---
+++ cosmetic.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+echo "Applying dark mode."
+echo "MacOS will ask permissions for this action the first time it's executed."
+osascript -e 'tell app "System Events" to tell appearance preferences to set dark mode to true'
+echo ""
+
+echo "Changing wallpaper..."
+echo ""
+echo "WARNING:"
+echo "This will only update the currently active desktop wallpapers."
+echo "To see the change on every virtual desktop, close and reopen the virtual desktops."
+echo ""
+
+osascript -e 'tell app "System Events" to tell every desktop to set picture to "~/macos-dotfiles/wallpaper.jpg"'
+
+echo "Wallpaper updated."
+echo ""
